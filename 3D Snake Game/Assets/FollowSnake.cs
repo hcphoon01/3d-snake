be240f9147f9442febdaa9f4b1fbecbbf50d751d
Harry Cameron <hcphoon01@gmail.com>
2019-03-22 19:32:55 +0800

    Implemented moving and camera tracking (both first and third person)

---
+++ 3D Snake Game/Assets/FollowSnake.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class FollowSnake : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 positionOffset;
+
+    private readonly float distanceBehindSnake = -5f;
+    private bool firstPerson;
+    
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey("c"))
+        {
+            firstPerson = true;
+        }
+
+        Quaternion rotationOffset = Quaternion.Euler(0, 90, 0);
+
+        if (firstPerson)
+        {
+            transform.position = player.position;
+            transform.rotation = player.rotation * rotationOffset;
+        }
+        else
+        {
+            positionOffset = new Vector3(GetXComponent(), positionOffset[1], -GetZComponent());
+
+            transform.position = player.position + positionOffset;
+
+            transform.rotation = player.rotation * rotationOffset;
+        }
+    }
+
+    private float GetXComponent()
+    {
+        return distanceBehindSnake * Mathf.Cos(Mathf.Deg2Rad * player.transform.eulerAngles.y);
+    }
+    private float GetZComponent()
+    {
+        return distanceBehindSnake * Mathf.Sin(Mathf.Deg2Rad * player.transform.eulerAngles.y);
+    }
+}
